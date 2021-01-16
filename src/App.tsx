176a426c73f3890b4bfa162e81ce6f8ec85507e3
dhoonjang <dhoonjang@gmail.com>
2021-01-16 18:14:28 +0900

    refac: router

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,38 @@ import React, { useMemo } from "react";
 import "./style/App.scss";
 import { useRecoilValue } from "recoil";
 import { authState } from "./context/authContext";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Login from "./component/Login";
 import Main from "./component/Main";
 import { BrowserRouter } from "react-router-dom";
+import Calendar from "./component/Calendar";
+import History from "./component/History";
+import List from "./component/List";
 
 function App() {
   const isAuth = useRecoilValue(authState);
 
   const routes = useMemo(() => {
     if (isAuth) {
-      return (
-        <Switch>
-          <Route exact path="/" component={Main} />
-        </Switch>
-      );
+      return [
+        <Route exact path="/" component={Main} />,
+        <Route path="/calendar" component={Calendar} />,
+        <Route path="/history" component={History} />,
+        <Route path="/list" component={List} />,
+      ];
     } else {
-      <Switch>
-        <Route path="/" component={Login} />
-      </Switch>;
+      return [<Route path="/" component={Login} />];
     }
   }, [isAuth]);
 
-  return <BrowserRouter>{routes}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        {routes}
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/component/List/index.tsx
+++ src/component/List/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./style/List/index.scss";
 
 function List() {
-  return <div className="List">메인 페이지</div>;
+  return <div className="List">고정지출 목록 페이지</div>;
 }
 
 export default List;
